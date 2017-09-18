7fe085a85a4a003af46fe7ef9337830254a6d07e
areyesq89 <alejandro.reyes.ds@gmail.com>
2017-09-18 19:25:37 -0400

    started vignette, added examples, documentation. tidy=TRUE and vectorization now works

---
+++ R/PerformanceMetrics.R
@@ -0,0 +1,254 @@
+#' @title Add a performance metric definition to a \code{\link{SummarizedBenchmark}} object.
+#' @aliases addPerformanceFunction
+#' @description
+#' This is a function to define performance metrics for benchmarking methods.
+#' The function is saved into the \code{performanceFunctions} slot.
+#'
+#' @param object A \code{\link{SummarizedBenchmark}} object.
+#' @param evalMetric A string with the name of the evaluation metric.
+#' @param assay A string with an assay name. Indicates the assay that should be
+#' given as input to this performance metric.
+#' @param evalFunction A function that calculates a performance metric. It should
+#' contain at least two arguments, query and truth, where query is the output vector
+#' of a method and truth is the vector of ground true values. If additional parameters
+#' are specified, they must contain default values.
+#'
+#' @examples
+#'
+#' data( sb )
+#' sb <- addPerformanceFunction(
+#'    object=sb,
+#'    assay="qvalue",
+#'    evalMetric="TPR",
+#'    evalFunction = function( query, truth, alpha=0.1 ){
+#'        goodHits <- sum( (query < alpha) & truth == 1 )
+#'        goodHits / sum(truth == 1)
+#'    }
+#' )
+#'
+#' @return A \code{\link{SummarizedBenchmark}} object.
+#' @importFrom tidyr gather
+#' @export
+#'
+addPerformanceFunction <- function( object, evalMetric, assay, evalFunction ){
+  validObject( object )
+  if( is.null( evalMetric ) ){
+    stop("Please specify a name for the new evaluation metric (parameter evalMetric).")
+  }
+  if( is.null( assay ) ){
+    stop("Please specify an assay for the new evaluation metric (parameter assay).")
+  }
+  if( !assay %in% names(object@performanceFunctions) ){
+    stop(sprintf( "Assay '%s' not found in this object", assay) )
+  }
+  if( !length( assay ) == 1 ){
+    stop("The 'assay' parameter must be of length 1")
+  }
+  if( !length( evalMetric ) == 1 ){
+    stop("The 'evalMetric' parameter must be of length 1")
+  }
+  object@performanceFunctions[[assay]][[evalMetric]] <- evalFunction
+  validObject( object )
+  object
+}
+
+
+#' @describeIn estimateMetrics Estimate performance metrics for a given assay
+#' @title Estimate performance metrics.
+#' @aliases estimateMetricsForAssay
+#' @description
+#' These functions estimate the performance metrics, either passed as arguments or
+#' added previously with the \code{\link{addPerformanceFunction}} function. The function
+#' will estimate the performance metric for each method.
+#'
+#' @param object A \code{\link{SummarizedBenchmark}} object.
+#' @param assay A string with an assay name. Indicates the assay that should be
+#' given as input to this performance metric.
+#' @param evalMetric A string with the name of the evaluation metric.
+#' @param evalFunction A function that calculates a performance metric. It should
+#' contain at least two arguments, query and truth, where query is the output vector
+#' of a method and truth is the vector of ground true values. If additional parameters
+#' are specified, they must contain default values. If this parameter is passed,
+#' the metrics in the object are ignored and only this evaluation metric
+#' is estimated.
+#' @param addColData Logical (default: FALSE). If TRUE, the results are added to the
+#' \code{\link{colData}} slot of the \code{\link{SummarizedExperiments}} object and
+#' the object is returned. If FALSE, only a \code{\link{DataFrame}} with the results
+#' is returned.
+#' @param tidy Logical (default: FALSE). If TRUE, a long formated \code{\link{data.frame}}
+#' is returned.
+#' @param ... Additional parameters passed to the performance functions.
+#'
+#' @examples
+#'
+#' data( sb )
+#' sb <- addPerformanceFunction(
+#'    object=sb,
+#'    assay="qvalue",
+#'    evalMetric="TPR",
+#'    evalFunction = function( query, truth, alpha=0.1 ){
+#'        goodHits <- sum( (query < alpha) & truth == 1 )
+#'        goodHits / sum(truth == 1)
+#'    }
+#' )
+#'
+#' qvalueMetrics <- estimateMetricsForAssay( sb, assay="qvalue" )
+#' allMetrics <- estimatePerformanceMetrics( sb )
+#' allMetricsTidy <- estimatePerformanceMetrics( sb, tidy=TRUE )
+#'
+#' @return Either a \code{\link{SummarizedBenchmark}} object, a \code{\link{DataFrame}} or
+#' a \code{\link{data.frame}}.
+#' @export
+#'
+estimateMetricsForAssay <- function( object, assay, evalMetric=NULL, addColData=FALSE,
+                                     evalFunction=NULL, tidy=FALSE, ...){
+  stopifnot( is( object, "SummarizedBenchmark" ) )
+  validObject( object )
+  if( !all( assay %in% names( assays( object ) ) ) ){
+    stop(sprintf("Assay '%s' not found in SummarizedBenchmark", assay))
+  }
+  if( !is.null(evalFunction) ){
+    object <- addPerformanceFunction( object, evalMetric, assay, evalFunction )
+
+  }
+  allFunctions <- object@performanceFunctions[[assay]]
+  if( length(allFunctions) == 0 ){
+    stop(sprintf( "Metric functions not specified for assay(s): %s. Check `?addPerformanceFunction`.", assay ) )
+  }
+  if( !is.null( evalMetric ) ){
+    if( !all(evalMetric %in% names(allFunctions)) ){
+      notDefined <- evalMetric[!evalMetric %in% names(allFunctions)]
+      stop(sprintf("Function for metric(s) %s is/are not defined", paste(notDefined, collapse=",")))
+    }
+    allFunctions <- allFunctions[names( allFunctions ) %in% evalMetric]
+  }
+  allDotArgs <- as.list( match.call( expand.dots=FALSE ) )[["..."]]
+  assayData <- assays(object)[[assay]]
+  assayTruth <- rowData(object)[,assay]
+  res <- lapply( names( allFunctions ), function( nf ){
+    f <- allFunctions[[nf]]
+    vecArgs <- formalArgs( f )[ !formalArgs( f ) %in% c("query", "truth") ]
+    passArgs <- list(truth=assayTruth)
+    eleMD <- DataFrame( colType="performanceMetric", assay=assay, performanceMetric=nf )
+    if( length( vecArgs ) > 0 ){
+      vf <- Vectorize( f, vectorize.args=vecArgs )
+      defaultArgs <- formals(f)[vecArgs[!vecArgs %in% names( passArgs )]]
+      extraDotArgs <- allDotArgs[names(allDotArgs) %in% formalArgs(f)]
+      if( length( extraDotArgs ) > 0 ){
+        for( i in names( extraDotArgs ) ){
+          defaultArgs[[i]] <- eval( extraDotArgs[[i]] )
+        }
+      }
+      passArgs <- c( passArgs, defaultArgs )
+      resNRow <- max( sapply( passArgs[vecArgs], length ) )
+      eleMD <- cbind( eleMD, DataFrame( as.data.frame( passArgs[vecArgs] ) ) )
+    }else{
+      vf <- f
+      resNRow <- 1
+    }
+    indRes <- sapply( seq_len( ncol( assayData ) ), function( i ){
+      assayRes <- do.call( vf, c( list(query=assayData[,i]), passArgs ) )
+      assayRes
+    } )
+    resDF <- DataFrame( t( matrix( indRes, nrow=resNRow ) ) )
+    elementMetadata( resDF ) <- eleMD
+    if( resNRow > 1 ){
+      colnames( resDF ) <- paste( nf, seq_len( ncol( resDF ) ), sep=".")
+    }else{
+      colnames( resDF ) <- nf
+    }
+    resDF
+  } )
+  names( res ) <- names( allFunctions )
+  res <- Reduce( cbind, res )
+  res <- cbind( colData( object ), res )
+  if( addColData | tidy ){
+    colData( object ) <- res
+    if( tidy ){
+      return( tidyUpMetrics( object ) )
+    }else{
+      return( object )
+    }
+  }else{
+    return( res )
+  }
+}
+
+#' @describeIn estimateMetrics Estimate performance metrics for all assays
+#' @aliases estimatePerformanceMetrics
+#' @export
+estimatePerformanceMetrics <- function( object, addColData=FALSE, tidy=FALSE, ... ){
+  stopifnot( is( object, "SummarizedBenchmark" ) )
+  validObject( object )
+  assayNames <- names( assays( object ) )
+  allRes <- lapply( assayNames, function(x){
+    if( length( object@performanceFunctions[[x]] ) > 0 ){
+      estimateMetricsForAssay( object, assay=x, ... )
+    }else{
+      NULL
+    }
+  } )
+  allRes <- allRes[!sapply(allRes, is.null)]
+  if( length( allRes ) > 0 ){
+    allRes <-
+      lapply( allRes,
+              function(x){
+                x[,elementMetadata( x )$colType == "performanceMetric",drop=FALSE]
+              } )
+    allRes <- Reduce( cbind, allRes )
+  }else{
+    stop("Metric functions not found for any assay. Check `?addPerformanceFunction`. to include these." )
+  }
+  allRes <- cbind( colData(object), allRes )
+  if( addColData | tidy ){
+    colData(object) <- allRes
+    if( tidy ){
+      return( tidyUpMetrics( object ) )
+    }else{
+      return( object )
+    }
+  }else{
+    return( allRes )
+  }
+}
+
+#' @title Reformat performance metrics to a long format.
+#' @aliases tidyUpMetrics
+#' @description
+#' This function takes as input a \code{SummarizedBenchmark} object, extracts the
+#' estimated performance metrics and reformats them into a long-formated data frame.
+#'
+#' @param object A \code{\link{SummarizedBenchmark}} object.
+#' @examples
+#' data( "sb", package="SummarizedBenchmark" )
+#' sb <- estimateMetricsForAssay( sb, assay="qvalue", evalMetric="rejections",
+#'     evalFunction=function( query, truth, alpha=0.1 ){
+#'         sum( query < alpha )
+#'     }
+#'     addColData=TRUE )
+#' tidyUpMetrics( sb )
+#'
+#' @return A tidy \code{data.frame}
+#' @importFrom tidyr gather
+#' @export
+#'
+tidyUpMetrics <- function( object ){
+  stopifnot(is( object, "SummarizedBenchmark" ) )
+  validObject( object )
+  res <- colData( object )
+  isPerformanceMetric <- elementMetadata( res )$colType == "performanceMetric"
+  if( !sum(isPerformanceMetric) > 0 ){
+    stop("No performance metrics were found. Check ?estimatePerformanceMetrics for further information")
+  }
+  valueCols <- colnames(res)[isPerformanceMetric]
+  tidyRes <- gather( as.data.frame(res), keys=valueCols )
+  mData <- as.data.frame( elementMetadata(res)[isPerformanceMetric,] )
+  rownames(mData) <- valueCols
+  mData[["colType"]] <- NULL
+  for( i in colnames(mData) ){
+    varAdd <- mData[[i]]
+    names(varAdd) <- rownames(mData)
+    tidyRes[[i]] <- varAdd[tidyRes$key]
+  }
+  tidyRes
+}
--- R/SummarizedBenckmark-class.R
+++ R/SummarizedBenckmark-class.R
@@ -1,12 +1,12 @@
-#' @describeIn SummarizedBenchmark
 #' @name SummarizedBenchmark-class
 #' @title SummarizedBenchmark class documentation
-#' @description Extension of the \code{\link{RangedSummarizedExperiment}} to
+#' @description
+#' Extension of the \code{\link{RangedSummarizedExperiment}} to
 #' store the output of different methods intended for the same purpose
 #' in a given dataset. For example, a differential expression analysis could be
 #' done using \pkg{limma}-voom, \pkg{edgeR} and \pkg{DESeq2}. The
 #' SummarizedBenchmark class provides a framework that is useful to store
-#' and compare the results from the three different programs.
+#' and compare the results from the three different software
 #' @slot performanceFunctions A \code{\link{SimpleList}} of the same length
 #' as the number of \code{\link{assays}} containing performance
 #' functions to be compared with the ground truths.
@@ -30,26 +30,86 @@ setValidity( "SummarizedBenchmark", function( object ) {
     stop("The names of the performanceFunctions list must match the names of the assays")
   }
   if( !all( sapply( object@performanceFunctions, "class" ) == "list" ) ){
-    stop("Each list element of of the performanceFunctions slot must be a list of functions")
+    stop("In the slot 'performanceFunctions', each element of the list must contain a list of functions")
   }
-  for( i in seq_len( length( object@performanceFunctions ) ) ){
-    goodArgs <- sapply( object@performanceFunctions, function(x){
-      if( length( x ) > 0 ){
-        sapply( x, function(y){
-          all( c("query", "truth") %in% formalArgs( y ))
-        } )
+  permFunc <- object@performanceFunctions
+  for( i in names( permFunc ) ){
+    funcAssay <- permFunc[[i]]
+    for(j in names(funcAssay) ){
+      y <- funcAssay[[j]]
+      f1 <- all( c("query", "truth") %in% formalArgs( y ) )
+      if( !f1 ){
+        stop(sprintf( "The performance function '%s' for assay '%s' does not contain a 'query' and/or a 'truth' argument", j, i) )
       }
-    } )
-  }
-  if( !is.null( unlist( goodArgs ) ) & !all( unlist( goodArgs ) ) ){
-    stop("All performance functions must contain a `query` and a `truth` argument")
+      otherArgs <- formals(y)[!names(formals(y)) %in% c("query", "truth")]
+      f2 <- any( sapply(otherArgs, class) == "name" )
+      if( f2 ){
+        missingDefaults <- names(otherArgs)[which(f2)]
+        stop(sprintf("The parameter(s) '%s' of the performance function '%s' for assay '%s' has/have no default values. Please specify defaults values.", paste(missingDefaults, collapse=","), j, i ) )
+      }
+    }
   }
   TRUE
 } )
 
 
-SummarizedBenchmark <- function( assays, colData, ftData=NULL, groundTruth=NULL,
-                                 performanceFunctions=NULL, ... ){
+#' @title Constructor function for SummarizedBenchmark objects.
+#' @aliases SummarizedBenchmark
+#' @description
+#' Function to construct \code{SummarizedBenchmark} objects.
+#'
+#' @param assays A list containing outputs of the methods to be
+#' benchmark. Each element of the list must contain a matrix or
+#' data.frame of n x m, n being the number of features tested
+#' (e.g. genes) and m being the number of methods in the benchmark.
+#' Each element of the list must contain a single assay
+#' (the outputs of the methods to be benchmarked). For example,
+#' for a benchmark of differential expression methods, one assay could contain
+#' the q-values from the different methods and another assay could be the
+#' estimated log fold changes.
+#' @param colData A \code{\link{DataFrame}} describing the annotation of the
+#' methods. These could include version of the software or the parameters
+#' used to run them.
+#' @param ftData A \code{\link{DataFrame}} object describing the rows. This parameter
+#' is equivalent to the parameter rowData of a SummarizedExperiment.
+#' @param groundTruth If present, a \code{\link{DataFrame}} containing the ground truths.
+#' If provided, the number of columns must be the same as the number of assays
+#' (NA's are accepted).
+#' The names of the columns should have the same names as the assays.
+#' @param performanceFunctions A \code{\link{SimpleList}} of the same
+#' length as the number of assays. Each element of the list must be a list
+#' of functions. Each function must contain the parameters 'query' and
+#' 'truth'.
+#' @param ... Additional parameters passed to \code{\link{SummarizedExperiment}}.
+#'
+#' @examples
+#'
+#' ## loading the example data from iCOBRA
+#' library(iCOBRA)
+#' data(cobradata_example)
+#'
+#' ## a bit of data wrangling and reformatting
+#' assays <- list(
+#'     qvalue=cobradata_example@padj,
+#'     logFC=cobradata_example@score )
+#' assays[["qvalue"]]$DESeq2 <- p.adjust(cobradata_example@pval$DESeq2, method="BH")
+#' groundTruth <- DataFrame( cobradata_example@truth[,c("status", "logFC")] )
+#' colnames(groundTruth) <- names( assays )
+#' colData <- DataFrame( method=colnames(assays[[1]]) )
+#' groundTruth <- groundTruth[rownames(assays[[1]]),]
+#'
+#' ## constructing a SummarizedBenchmark object
+#' sb <- SummarizedBenchmark(
+#'     assays=assays, colData=colData,
+#'     groundTruth=groundTruth, ftData=ftData )
+#'
+#' @return A \code{\link{SummarizedBenchmark}} object.
+#' @importFrom tidyr gather
+#' @importFrom S4Vectors DataFrame
+#' @export
+#'
+SummarizedBenchmark <- function( assays, colData, ftData=NULL,
+                                 groundTruth=NULL, performanceFunctions=NULL, ... ){
   if( is( colData, "data.frame" ) ){
     colData <- DataFrame( colData )
   }
@@ -88,199 +148,8 @@ SummarizedBenchmark <- function( assays, colData, ftData=NULL, groundTruth=NULL,
   }
   elementMetadata( colData ) <- DataFrame( colType="methodInformation" )
   se <- as( SummarizedExperiment( assays, colData=colData, ... ),
-          "RangedSummarizedExperiment")
+            "RangedSummarizedExperiment")
   rowData(se) <- rData
   sb <- new( "SummarizedBenchmark", se, performanceFunctions=performanceFunctions )
   sb
 }
-
-addPerformanceFunction <- function( object, evalMetric, assay, FUN ){
-  if( !all( c("query", "truth") %in% formalArgs( FUN ) ) ){
-    stop("All performance functions need to have both a `query` and a `truth` argument")
-  }
-  if( is.null( evalMetric ) ){
-    stop("Please specify a name for the new evaluation metric (parameter evalMetric).")
-  }
-  if( is.null( assay ) ){
-    stop("Please specify an assay for the new evaluation metric (parameter assay).")
-  }
-  object@performanceFunctions[[assay]][[evalMetric]] <- FUN
-  validObject( object )
-  object
-}
-
-estimateMetricsForAssay <- function( object, assay, evalMetric=NULL, addColData=FALSE, evalFunction=NULL, ...){
-  stopifnot( is( object, "SummarizedBenchmark" ) )
-  stopifnot( all( assay %in% names( assays( object ) ) ) )
-  if( !is.null(evalFunction) ){
-    object <- addPerformanceFunction( object, evalMetric, assay, evalFunction )
-
-  }
-  allFunctions <- object@performanceFunctions[[assay]]
-  if( length(allFunctions) == 0 ){
-    stop(sprintf( "Metric functions not specified for assay(s): %s. Check `?addPerformanceFunction`.", assay ) )
-  }
-  if( !is.null( evalMetric ) ){
-    if( !all(evalMetric %in% names(allFunctions)) ){
-      notDefined <- evalMetric[!evalMetric %in% names(allFunctions)]
-      stop(sprintf("Function could not be found for metric(s) %s", paste(notDefined, collapse=",")))
-    }
-    allFunctions <- allFunctions[names( allFunctions ) %in% evalMetric]
-  }
-  allDotArgs <- as.list( match.call( expand.dots=FALSE ) )[["..."]]
-  assayData <- assays(object)[[assay]]
-  assayTruth <- rowData(object)[,assay]
-  res <- lapply( names( allFunctions ), function( nf ){
-    f <- allFunctions[[nf]]
-    vecArgs <- formalArgs( f )[ !formalArgs( f ) %in% c("query", "truth") ]
-    passArgs <- list(truth=assayTruth)
-    eleMD <- DataFrame( colType="performanceMetric", assay=assay, performanceMetric=nf )
-    if( length( vecArgs ) > 0 ){
-      vf <- Vectorize( f, vectorize.args=vecArgs )
-      defaultArgs <- formals(f)[vecArgs[!vecArgs %in% names( passArgs )]]
-      extraDotArgs <- allDotArgs[names(allDotArgs) %in% formalArgs(f)]
-      if( length( extraDotArgs ) > 0 ){
-        for( i in names( extraDotArgs ) ){
-          defaultArgs[[i]] <- eval( extraDotArgs[[i]] )
-        }
-      }
-      passArgs <- c( passArgs, defaultArgs )
-      resNRow <- max( sapply( passArgs[vecArgs], length ) )
-      eleMD <- cbind( eleMD, DataFrame( as.data.frame( passArgs[vecArgs] ) ) )
-    }else{
-      vf <- f
-      resNRow <- 1
-    }
-    indRes <- sapply( seq_len( ncol( assayData ) ), function( i ){
-      assayRes <- do.call( vf, c( list(query=assayData[,i]), passArgs ) )
-      assayRes
-    } )
-    resDF <- DataFrame( t( matrix( indRes, nrow=resNRow ) ) )
-    elementMetadata( resDF ) <- eleMD
-    if( resNRow > 1 ){
-      colnames( resDF ) <- paste( nf, seq_len( ncol( resDF ) ), sep=".")
-    }else{
-      colnames( resDF ) <- nf
-    }
-    resDF
-  } )
-  names( res ) <- names( allFunctions )
-  res <- Reduce( cbind, res )
-  res <- cbind( colData( object ), res )
-  if( addColData ){
-    colData( object ) <- res
-    return( object )
-  }else{
-    return( res )
-  }
-}
-
-estimatePerformanceMetrics <- function( object, addColData=FALSE, ... ){
-  stopifnot( is( object, "SummarizedBenchmark" ) )
-  assayNames <- names( assays( object ) )
-  allRes <- lapply( assayNames, function(x){
-    if( length( object@performanceFunctions[[x]] ) > 0 ){
-      estimateMetricsForAssay( object, assay=x, ... )
-    }else{
-      NULL
-    }
-  } )
-  allRes <- allRes[!sapply(allRes, is.null)]
-  if( length( allRes ) > 0 ){
-    allRes <- 
-      lapply( allRes, 
-        function(x){ 
-          x[,elementMetadata( x )$colType == "performanceMetric",drop=FALSE]
-        } )
-    allRes <- Reduce( cbind, allRes )
-  }else{
-    stop("Metric functions not found for any assay. Check `?addPerformanceFunction`. to include these." )
-  }
-  allRes <- cbind( colData(object), allRes )
-  if( addColData ){
-    colData(object) <- allRes
-    return( object )
-  }else{
-    return( allRes )
-  }
-}
-
-
-library(iCOBRA)
-data(cobradata_example)
-
-cobradata_example[,1]
-
-assays <- list(
-  qval=cobradata_example@padj,
-  logFC=cobradata_example@score )
-assays[["qval"]]$DESeq2 <- p.adjust(cobradata_example@pval$DESeq2, method="BH")
-groundTruth <- DataFrame( cobradata_example@truth[,c("status", "logFC")] )
-colnames(groundTruth) <- names( assays )
-ftData <- NULL
-colData <- DataFrame( method=colnames(assays[[1]]) )
-performanceFunctions <- NULL
-groundTruth <- groundTruth[rownames(assays[[1]]),]
-
-### replace assay with: result? output? metric?
-
-sb <- SummarizedBenchmark(
-  assays=assays, colData=colData,
-  groundTruth=groundTruth, ftData=ftData )
-
-estimatePerformanceMetrics(sb)
-
-sb <- addPerformanceFunction(
-  object=sb,
-  assay="qval",
-  evalMetric="TPR",
-  FUN = function( query, truth, alpha=0.1 ){
-    goodHits <- sum( (query < alpha) & truth == 1 )
-    goodHits / sum(truth == 1)
-    }
-)
-
-sb <- addPerformanceFunction(
-  object=sb,
-  evalMetric="jaccardIndex",
-  assay="qval",
-  FUN = function( query, truth, alpha=0.1){
-    goodHits <- sum( xor( query < alpha, as.logical( truth ) ) )
-    goodHits / length( truth )
-  }
-)
-
-estimatePerformanceMetrics( sb, alpha=c(0.1, 0.2, 0.3) )
-
-sb <- addPerformanceFunction(
-  object=sb,
-  evalMetric="EucledianDist",
-  assay="logFC",
-  FUN <- function( query, truth ){
-    sqrt( sum( (query - truth)^2 ) )
-  }
-)
-
-## add option `tidy=TRUE` for estimateMetrics*
-
-colData( estimateMetricsForAssay( sb, evalMetric="TPR", assay="qval", alpha=c( 0.1, 0.2, 0.3, 0.4 ), addColData=TRUE ) )
-
-estimateMetricsForAssay( sb, evalMetric="TPR", assay="qval", alpha=0.1 )
-
-
-t( matrix(estimateMetricsForAssay( sb, assay="qval", alpha=0.1)[[1]], nrow=1 ) )
-
-estimateMetricsForAssay( sb, assay="logFC" )
-
-estimateMetricsForAssay(
-  sb,
-  assay="qvalue",
-  evalMetric="rejections",
-  evalFunction=function(query, truth, alpha=0.1){
-    sum( query < alpha )
-  } )
-
-
-elementMetadata( estimatePerformanceMetrics( sb, alpha=c(0.1, 0.2, 0.3) ) )
-
-estimateMetricsForAssay( sb, assay="logFC" )[,2,drop=FALSE]
