4549e632009375ae5ff5cece09c1a0074c4a691f
areyesq89 <alejandro.reyes.ds@gmail.com>
2018-01-17 08:34:30 -0500

    Moved some vignette sections to 'Appendix' section.

---
+++ R/data-soneson2016.R
@@ -0,0 +1,7 @@
+#' Data objects containing counts and ground truths of the simulations from Soneson et al., 2016.
+#'
+#' @name truthdat
+#' @aliases truthdat txi
+#' @docType data
+#' @keywords data
+NULL
