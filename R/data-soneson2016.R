4951aadda05a1db0b5f48cafd37228272bd92d14
areyesq89 <alejandro.reyes.ds@gmail.com>
2018-03-15 07:39:09 -0400

    Removed broken links from documentation.

--- R/data-soneson2016.R
+++ R/data-soneson2016.R
@@ -1,10 +1,10 @@
 #' SummarizedBenchmark example
 #'
 #' @name sb
-#' @description This object contains the example data from the \code{\link{iCOBRA}} package reformatted as a
-#' \code{SummarizedBenchmark} object. It consists of differential expression results from \code{\link{DESeq2}}
-#' \code{\link{edgeR}} and \code{\link{limma}}-voom.
-#' @source Example data from the \code{\link{iCOBRA}} package.
+#' @description This object contains the example data from the \code{iCOBRA} package reformatted as a
+#' \code{SummarizedBenchmark} object. It consists of differential expression results from \code{DESeq2}
+#' \code{edgeR} and \code{limma}-voom.
+#' @source Example data from the \code{iCOBRA} package.
 #' @aliases truthdat txi
 #' @docType data
 #' @keywords data SummarizedBenchmark
