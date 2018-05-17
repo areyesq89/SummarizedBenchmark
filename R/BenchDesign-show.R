f09ff7b6d664a8942e0708b931b21768e2ed8fe9
Patrick Kimes <patrick.kimes@gmail.com>
2018-05-17 17:14:34 -0400

    Fix indentation in print output

--- R/BenchDesign-show.R
+++ R/BenchDesign-show.R
@@ -55,7 +55,7 @@ setMethod("show", signature(object = "BDMethod"), .show.BDMethod)
         if (dtype == "data")
             cat("    names:", paste(names(object@data@data), collapse=", "), "\n")
         if (dtype == "md5hash")
-            cat(" MD5 hash:", stringr::str_trunc(object@data@data, 50), "\n")
+            cat("    MD5 hash:", stringr::str_trunc(object@data@data, 50), "\n")
     } else {
         cat("    NULL\n")
     }
